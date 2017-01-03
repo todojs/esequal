f41f03b492466d07a4062473fb6690b7cb3b76d3
Pablo Almunia <Pablo Almunia>
2017-01-03 18:14:05 +0100

    Non strict comparison

--- esequal.js
+++ esequal.js
@@ -2,12 +2,22 @@
 (function (root) {
     "use strict";
 
-    function equal(a, b) {
-        if (a === b) {
-            return true;
+    function equal(a, b, options) {
+        options = options || {};            // Optional parameter
+        if (a === b) {                      // Strict comparison
+            return equal.VALUE_AND_TYPE;    // Equal value and type
         }
-        return false;
+        /* jshint -W116 */
+        if (options.nonStrict && a == b) {  // Non strict comparison (optional)
+            return equal.VALUE;             // Equal value (different type)
+        }
+        /* jshint +W116 */
+        return equal.NOT_EQUAL;             // Not equal
     }
+    equal.NOT_EQUAL           = 0;
+    equal.VALUE               = 1;
+    equal.VALUE_AND_TYPE      = 2;
+
 
     // Export for node and browser
     if (typeof exports !== 'undefined') {
